3711b56d940ba72265b2e80e5950d96bd4762d14
李铭昕 <715557344@qq.com>
2021-07-19 23:03:05 +0800

    增加Inject常规测试

---
+++ app/Service/BarService.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace App\Service;
+
+use Hyperf\Contract\StdoutLoggerInterface;
+use Hyperf\Di\Annotation\Inject;
+use Psr\Container\ContainerInterface;
+
+class BarService
+{
+    #[Inject]
+    public ContainerInterface $container;
+
+    public function __construct(public StdoutLoggerInterface $logger)
+    {
+    }
+}
